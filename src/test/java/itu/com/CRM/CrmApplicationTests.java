05b35a81f1386cc215d50279cd6f91d5f7ff2fe8
Mahandry <155745162+MahandryAndrianantenaina@users.noreply.github.com>
2025-03-22 12:51:03 +0300

    Initialisation du projet

---
+++ src/test/java/itu/com/CRM/CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package itu.com.CRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
